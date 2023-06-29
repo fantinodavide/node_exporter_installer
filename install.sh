c9dd4259d0e09eb1ebb3972472c9b6d0cda00e22
Fantino Davide <fantinodavide02@gmail.com>
2023-06-30 01:17:41 +0200

    fix: rm typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ mkdir /opt/node_exporter
 cd /opt/node_exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.6.0/node_exporter-1.6.0.linux-amd64.tar.gz
 tar xvfz node_exporter-*.*-amd64.tar.gz -C ./
-rn node_exporter-*.*-amd64.tar.gz
+rm node_exporter-*.*-amd64.tar.gz
 wget https://raw.githubusercontent.com/fantinodavide/node_exporter_installer/main/node_exporter.service;
 mv node_exporter.service /etc/systemd/system/
 systemctl enable --now node_exporter.service

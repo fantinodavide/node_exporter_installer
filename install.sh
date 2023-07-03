64b6267a67c8bcc9bf6e14edc4bdf760e3fb3ff0
Davide Fantino <80767709+fantinodavide@users.noreply.github.com>
2023-07-03 21:12:05 +0200

    fix: service filename

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ tar xvfz node_exporter-*.*-amd64.tar.gz -C ./
 mv node_exporter-*.*-amd64/* ./
 rm node_exporter-*.*-amd64.tar.gz
 rmdir node_exporter-*.*-amd64
-wget https://raw.githubusercontent.com/fantinodavide/node_exporter_installer/main/node_exporter.service?$RANDOM
+wget -O node_exporter.service https://raw.githubusercontent.com/fantinodavide/node_exporter_installer/main/node_exporter.service?$RANDOM
 mv node_exporter.service /etc/systemd/system/
 systemctl enable --now node_exporter.service
 systemctl status node_exporter.service
